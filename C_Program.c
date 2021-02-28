db6a540e827c6bc3190d361ac409a422be2a139b
Ubuntu <vveenamsgmail@vveenamsgmail.1qnwer0prfwuxcpyznpvettsxa.gx.internal.cloudapp.net>
2021-02-28 05:28:29 +0000

    i am pushing file

---
+++ C_Program.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+printf("hello i am in c program");
+return 0;
+}
---
+++ Hello.java
@@ -0,0 +1,5 @@
+class HelloJava{
+public static void main(String args[]){
+System.out.println("i am java program");
+}
+}
